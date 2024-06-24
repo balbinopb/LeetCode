ee1c35c53d8df589c4bc0f1d1a7b28c50b754cc4
Balbino Pedro Baptista <dinubatista4@gmail.com>
2024-06-25 01:02:07 +0700

    #4

---
+++ searchInsert.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func searchInsert(nums []int, target int) int {
+    start := 0
+    end := len(nums) - 1
+    
+    for start <= end {
+        mid := (start + end) / 2
+        if nums[mid] == target {
+            return mid
+        } else if nums[mid] > target {
+            end = mid - 1
+        } else {
+            start = mid + 1
+        }
+    }
+    
+    return start
+}
+
+func main(){
+	nums:= []int {1,3,5,6}
+	target := 5
+
+	fmt.Println(searchInsert(nums,target))
+}
