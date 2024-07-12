fad4c556dbc25f72a5d700b1027ffc0192923a1e
balbinopb <dinubatista4@gmail.com>
2024-07-12 23:58:20 +0700

    #10

---
+++ countNegative.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func countNegatives(grid [][]int) int {
+	sum := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] < 0 {
+				sum++
+			}
+		}
+	}
+	return sum
+}
+
+func main() {
+	grid := [][]int{
+		{4, 3, 2, -1},
+		{3, 2, 1, -1},
+		{1, 1, -1, -2},
+		{-1, -1, -2, -3},
+	}
+	fmt.Println(countNegatives(grid)) 
+}
