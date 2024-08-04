e9ae8b4f948e5ec853bd54e988909a40abf64b36
balbinopb <dinubatista4@gmail.com>
2024-08-04 18:20:57 +0700

    #12

---
+++ lengthLastWord.go
@@ -0,0 +1,23 @@
+
+package main
+
+import"fmt"
+
+func lengthOfLastWord(s string) int {
+    length := 0
+    inWord := false
+
+    for i := len(s) - 1; i >= 0; i-- {
+        if s[i] != ' ' {
+            length++
+            inWord = true
+        } else if inWord {
+            break
+        }
+    }
+    
+    return length
+}
+func main(){
+	fmt.Println(lengthOfLastWord("helloo world!"))
+}
